989ebda34e7e178c01fdc43ca59583a232ca608f
Kim-2301 <seunghhhh2344@gmail.com>
2024-04-30 01:57:16 +0900

    [Chore] Add urlpatterns & swagger schema
    issue_id : DE-22,23,24

--- dopany/dopany/urls.py
+++ dopany/dopany/urls.py
@@ -1,23 +1,31 @@
-"""
-URL configuration for dopany project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# schema_url_patterns = [ 
+#     path('', include('ETF.urls')),
+#     ]
+schema_view = get_schema_view(
+   openapi.Info(
+      title="TrendyDE-Dopany",
+      default_version='v1',
+      description="도메인 기반 채용 공고 조회 사이트 API 문서",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+#    patterns=schema_url_patterns,
+)
 
 urlpatterns = [
+    #swagger
+    re_path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    
+    #apps
     path("admin/", admin.site.urls),
-    path("etf/",include('etf.urls'))
+    path("etf/",include('ETF.urls')),
 ]
