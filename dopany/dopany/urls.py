6ee3d68d2c52e5ee06d49694d5ac0ebd197622c2
Kim-2301 <seunghhhh2344@gmail.com>
2024-05-01 20:31:42 +0900

    [Chore] Link to Swagger

--- dopany/dopany/urls.py
+++ dopany/dopany/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include,re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="TrendyDE-Dopany",
+      default_version='v1',
+      description="도메인 기반 채용 공고 조회 사이트 API 문서",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    #swagger
+    re_path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    
+    #apps
     path("admin/", admin.site.urls),
+    path("etf/",include('ETF.urls')),
 ]
