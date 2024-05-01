c4ebe9547132cbbeb6bf7ce72f2a1590bd4c7e55
Kim-2301 <seunghhhh2344@gmail.com>
2024-05-02 00:36:54 +0900

    [Feat] Add EtfMajorCompany table

--- dopany/ETF/models.py
+++ dopany/ETF/models.py
@@ -3,15 +3,13 @@
 from django.db import models
 
 '''
-Domain [icon: user, color: blue] {
+Domain {
     domain_id       int         pk
     created_at      time
     updated_at      time
     domain_name     string      unique
-    etf_id          string      fk
 }
 '''
-
 class Domain(models.Model):
     domain_id = models.AutoField(primary_key=True)
     domain_name = models.CharField(max_length=100, unique=True)
@@ -20,27 +18,44 @@ class Domain(models.Model):
     
 
 '''
-Etf [icon: user, color: blue] {
-    etf_id              int         pk
-    created_at          time
-    updated_at          time
-    transaction_date    time        unique
-    closing_price       int
-    trading_volume      int
-    change              float
-    technical_analysis  float
-    domain_id           int         fk
+Etf  {
+    etf_id              int         pk          자동생성(ex. 행번호)
+    etf_name	        string                  ETF 상품명(ex. KODEX K-메타버스액티브)
+    created_at          time                    레코드 생성 시간
+    updated_at          time                    레코드 수정 시간
+    transaction_date    time                    거래일
+    closing_price       int                     종가
+    trading_volume      int                     거래량
+    change              float                   변동(%)_전일 종가 대비 당일 종가
+    domain_id           int         fk          도메인 id
 }
 '''
-
 class Etf(models.Model):
     etf_id = models.AutoField(primary_key=True)
+    etf_name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    transaction_date = models.DateTimeField(unique=True)
+    transaction_date = models.DateTimeField
     closing_price = models.IntegerField()
     trading_volume = models.IntegerField()
     change = models.FloatField()
-    technical_analysis = models.FloatField()
-    domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    domain_id = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ['etf_name', 'transaction_date']
+    
 
+'''
+EtfMajorCompany {
+    eff_major_company_id	int         pk      자동생성(ex. 행번호)
+    created_at	            time                레코드 생성 시간
+    updated_at	            time                레코드 수정 시간
+    company_name	        string              ETF 상품 주요 구성자산 기업이름(ex. CJ ENM)
+    etf_name                string      fk      ETF 상품명
+}
+'''
+class EtfMajorCompany(models.Model):
+    etf_major_company_id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now=True)
+    company_name = models.CharField(max_length=100, unique=True)
+    etf_name = models.ForeignKey(Etf, on_delete=models.CASCADE)
--- dopany/ETF/serializers.py
+++ dopany/ETF/serializers.py
@@ -1,11 +1,22 @@
 # serializers.py
 
 from rest_framework import serializers
-from .models import Etf
+from .models import Etf, EtfMajorCompany, Domain
+
+class Domain(serializers.ModelSerializer):
+    class Meta:
+        model = Domain
+        fields = '__all__'
 
 class EtfSerializer(serializers.ModelSerializer):
     class Meta:
         model = Etf
-        fields = ['transaction_date', 'closing_price']
+        fields = '__all__'
+
+
+class EtfMajorCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EtfMajorCompany
+        fields = '__all__'
         
 
