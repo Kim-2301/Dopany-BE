eb81ad401d9e22c2ccf99f9ef4a17f8678b17a1f
cshiz <64184518+cshiz9206@users.noreply.github.com>
2024-05-06 11:15:00 +0900

    [feat] complete components design
    Resolves:
    - complete design domain index page
    -----------------------------
    Required:
    + API test
    -----------------------------
    issue_id: DE-26

--- dopany/ETF/static/ETF/js/company_container_script.js
+++ dopany/ETF/static/ETF/js/company_container_script.js
@@ -1,59 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const mainContainer = document.getElementById("company-body-section");
+  const mainContainer = document.getElementById("company-grid-section");
+  mainContainer.innerHTML = "";
 
-  for (let i = 0; i < 10; i++) {
-    const lineContainer = document.createElement("div");
-    lineContainer.className = "line-container"; // Ensuring unique ID for each line-container
-    for (let i = 0; i < 5; i++) {
-      const companyContainer = document.createElement("div");
-      companyContainer.id = "company-container";
-      companyContainer.style.zIndex = "10";
-      companyContainer.innerHTML = `
-            <div id="company-icon-section"></div>
-            <div id="company-content-section">
-                <div id="title-article"></div>
-                <div id="description-article"></div>
+  // for (let i = 0; i < 10; i++) {
+  //   const lineContainer = document.createElement("div");
+  //   lineContainer.className = "line-container"; // Ensuring unique ID for each line-container
+  for (let i = 0; i < 5; i++) {
+    const companyContainer = document.createElement("button");
+    companyContainer.className = "company-container";
+    companyContainer.style.zIndex = "10";
+    companyContainer.innerHTML = `
+            <div class="company-icon-section">
+              <div style="width: 50%;">
+                <img src="/static/ETF/images/Dopany.svg" alt="icon SVG" style="max-width: 100%; height: auto;">
+              </div>
+            </div>
+            <div class="company-content-section">
+                <div id="company-title-article">
+                  회사명
+                </div>
+                <div>
+                  <ul class="company-description-article" style="margin: 0">
+                    <li>산업분야</li>
+                    <li>매출액</li>
+                    <li>규모</li>
+                  </ul>
+                </div>
             </div>
         `;
-      lineContainer.appendChild(companyContainer);
-    }
+    mainContainer.appendChild(companyContainer);
+    // }
 
-    mainContainer.appendChild(lineContainer);
+    // mainContainer.appendChild(lineContainer);
   }
 });
 
-// document.addEventListener("DOMContentLoaded", function () {
+// function adjustCardLayout() {
+//   const screenWidth = window.innerWidth;
+//   let columns;
+//   if (screenWidth < 600) {
+//     columns = 1; // 화면이 좁을 때는 한 행에 한 카드
+//   } else if (screenWidth < 900) {
+//     columns = 2; // 중간 크기 화면에서는 한 행에 두 카드
+//   } else {
+//     columns = 3; // 더 넓은 화면에서는 한 행에 세 카드
+//   }
 //   const mainContainer = document.getElementById("company-body-section");
+//   mainContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+// }
 
-//   if (!mainContainer) {
-//     console.error("The main container element does not exist!");
-//     return;
-//   }
-
-//   function createCompanyContainer() {
-//     const companyContainer = document.createElement("div");
-//     companyContainer.className = "company-container";
-//     companyContainer.style.zIndex = "10";
-//     companyContainer.innerHTML = `
-//           <div class="company-icon-section"></div>
-//           <div class="company-content-section">
-//               <div class="title-article"></div>
-//               <div class="description-article"></div>
-//           </div>
-//       `;
-
-//     return companyContainer;
-//   }
-
-//   for (let i = 0; i < 10; i++) {
-//     const lineContainer = document.createElement("div");
-//     lineContainer.className = "line-container";
-//     lineContainer.style.zIndex = "1";
-
-//     for (let j = 0; j < 5; j++) {
-//       lineContainer.appendChild(createCompanyContainer());
-//     }
-
-//     mainContainer.appendChild(lineContainer);
-//   }
-// });
+// window.addEventListener("resize", adjustCardLayout); // 창 크기 조정 시 레이아웃 조정
+// adjustCardLayout(); // 페이지 로드 시 레이아웃 초기화
