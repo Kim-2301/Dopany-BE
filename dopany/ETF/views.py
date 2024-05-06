505a4b1df704158d1ce9d7aaca040cf1ce4420c8
Kim-2301 <seunghhhh2344@gmail.com>
2024-05-06 19:01:21 +0900

    [Test] Complete API-2 test

--- dopany/ETF/views.py
+++ dopany/ETF/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-from .models import Etf, Domain, EtfMajorCompany
+from .models import EtfPrice, EtfProduct, Domain, EtfMajorCompany
 from django.db.models import Avg
 from django.core.exceptions import ObjectDoesNotExist
+import logging
 
 class ETFInfoView(APIView):
     @swagger_auto_schema(
@@ -13,46 +14,53 @@ class ETFInfoView(APIView):
         responses={200: 'Success', 400: 'Bad Request', 500: 'Internal Server Error'},
         manual_parameters=[
             openapi.Parameter('domain-name', openapi.IN_QUERY, description='Domain name', type=openapi.TYPE_STRING),
-            openapi.Parameter('time-unit', openapi.IN_QUERY, description='Time unit (day, month, year)', type=openapi.TYPE_STRING),
+            openapi.Parameter('time-unit', openapi.IN_QUERY, description='Time unit (day, week, month)', type=openapi.TYPE_STRING),
         ],
-        
     )
     def get(self, request):
         try:
-            # request에서 domian_name과 time_unit get
-            domain_name = request.GET.get('domain-name','IT')   #default는 IT
-            time_unit = request.GET.get('time-unit','day')      #default는 day
-
-            if time_unit not in ['day', 'month', 'year']:
-                return Response({"message": "잘못된 요청입니다."}, status=status.HTTP_400_BAD_REQUEST)
-
+            # request에서 domain_name과 time_unit get
+            domain_name = request.GET.get('domain-name', 'IT')   # default는 IT
+            time_unit = request.GET.get('time-unit', 'day')      # default는 day
+            
             # domain_name에 속하는 Etf정보 get
             domain = Domain.objects.get(domain_name=domain_name)
-            etf_data = Etf.objects.filter(domain=domain)
 
-            # time_unit별 closing_price 계산
-            # if time_unit == 'day':
-            #     etf_info = etf_data.values('transaction_date').annotate(closing_price=Avg('closing_price'))
-            # elif time_unit == 'month':
-            #     etf_info = etf_data.extra({'month': "EXTRACT(month FROM transaction_date)"}).values('month').annotate(closing_price=Avg('closing_price'))
-            # elif time_unit == 'year':
-            #     etf_info = etf_data.extra({'year': "EXTRACT(year FROM transaction_date)"}).values('year').annotate(closing_price=Avg('closing_price'))
-
-
-            # etf_info에 들어갈 요소 처리
+            etf_products = EtfProduct.objects.filter(domain=domain)
             etf_info = []
-            for etf in etf_data:
-                major_companies = EtfMajorCompany.objects.filter(etf_name=etf.etf_name).values_list('company_name', flat=True)
+
+            for etf_product in etf_products:
+                # 해당 ETF의 모든 거래 정보 가져오기
+                etf_prices = EtfPrice.objects.filter(etf_product=etf_product)
+                
+                transaction_dates = []
+                closing_prices = []
+
+                for etf_price in etf_prices:
+                    transaction_dates.append(etf_price.transaction_date.strftime('%Y-%m-%d'))
+                    closing_prices.append(etf_price.closing_price)
+                    major_companies = EtfMajorCompany.objects.filter(etf_product=etf_product).values_list('company_name', flat=True)
+                    
+                if time_unit == 'week':
+                    etf_avg = etf_prices.extra({'week': "EXTRACT(week FROM transaction_date)"}).values('week').annotate(time_avg=Avg('closing_price')).order_by('week')
+                elif time_unit == 'month':
+                    etf_avg = etf_prices.extra({'month': "EXTRACT(month FROM transaction_date)"}).values('month').annotate(time_avg=Avg('closing_price')).order_by('month')
+                else:
+                    etf_avg = None
+                    
+                # ETF 정보를 딕셔너리로 저장하고 리스트에 추가
                 etf_info.append({
-                    "etf_name": etf.etf_name,
+                    "etf_name": etf_product.etf_product_name,
                     "etf_major_company": list(major_companies),
-                    "transaction_date": etf.transaction_date.strftime('%Y-%m-%d'),
-                    "closing_price": etf.closing_price
+                    "transaction_dates": transaction_dates,
+                    "closing_prices": closing_prices,
+                    "time_avg" : etf_avg
                 })
+                
 
             response_data = {
                 "etf_info": etf_info,
-                "time_unit": time_unit
+                "time_unit": time_unit,
             }
             
             return Response(response_data, status=status.HTTP_200_OK)
